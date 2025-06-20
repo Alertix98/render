95c809046b477c9a7bdaf4c9eeb4bf1ec6f04ab8
Alertix98 <alessiocammarano@hotmail.com>
2025-06-20 10:22:24 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,23 @@
+import http from "http";
+import httpProxy from "http-proxy";
+
+const TARGET = "ws://nitro.blumhost.net:2805";
+
+const proxy = httpProxy.createProxyServer({
+  target: TARGET,
+  ws: true
+});
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("WebSocket proxy is running\n");
+});
+
+server.on("upgrade", (req, socket, head) => {
+  proxy.ws(req, socket, head);
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Proxy listening on port ${PORT}`);
+});
